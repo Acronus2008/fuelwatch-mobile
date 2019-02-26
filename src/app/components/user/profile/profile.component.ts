97b51d667b9e4c1a0556b018b4c550c40cc6c508
Alejandro <acronus2008@gmail.com>
2019-02-25 21:02:50 -0500

    getting data when the user is logged

--- src/app/components/user/profile/profile.component.ts
+++ src/app/components/user/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  private DEFAULT_UER_IMAGE: './assets/user_picture.png';
 
-  ngOnInit() {
+  userImage: any;
+  userName: any;
+  userEmail: any;
+
+  constructor(private router: Router, private route: ActivatedRoute) {
+
+      if (this.router.getCurrentNavigation().extras && this.router.getCurrentNavigation().extras.state) {
+          this.setUserProfileData(this.router.getCurrentNavigation().extras.state);
+      }
+
+  }
+
+    private setUserProfileData(routerData) {
+        this.userName = '';
+        this.userEmail = '';
+        this.userImage = this.DEFAULT_UER_IMAGE;
+
+        if (routerData.name) {
+            this.userName = routerData.name.replace(' ', '_').toLowerCase();
+        }
+
+        if (routerData.email) {
+            this.userEmail = routerData.email;
+        }
+
+        if (routerData.picture) {
+            this.userImage = routerData.picture;
+        }
+    }
+
+    ngOnInit() {
   }
 
 }
