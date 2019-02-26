5e0a1872e995d8f903089bb80d3480b5decc6b85
Alejandro <acronus2008@gmail.com>
2019-02-25 21:03:29 -0500

    adding environments variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
   production: true
 };
+
+export const FACEBOOK_API_URL = 'https://graph.facebook.com/';
+export const FACEBOOK_PERMISSIONS = ['public_profile', 'email'];
+export const FUELWATCH_RSS_URL = 'https://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS';
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,16 @@ export const environment = {
   production: false
 };
 
+export const FACEBOOK_API_URL = 'https://graph.facebook.com/';
+export const FACEBOOK_PERMISSIONS = ['public_profile', 'email'];
+export const FUELWATCH_RSS_URL = 'https://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS';
+
+/*
+* In development mode replace the FUELWATCH_RSS_URL for
+* http://localhost:3002/fuelWatchRSS/Suburb/Albany/Surrounding?Product=1&Suburb=Albany&Surrounding=no'
+* Requirements for that? Make a mock from RSS in your localhost environment
+* */
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
